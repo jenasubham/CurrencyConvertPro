48a4d514e94a5a59f8d62013dec7ee0273ea3997
Subham Jena <subhamjena4519@gmail.com>
2023-10-26 22:59:14 +0530

    Initial Commit

---
+++ src/components/Index.js
@@ -0,0 +1,6 @@
+// All the imports will come here and we can export all the imports directly, 
+// so that in the App file there will not be mess like so many import lines.
+
+import InputBox from './InputBox';
+
+export {InputBox};
